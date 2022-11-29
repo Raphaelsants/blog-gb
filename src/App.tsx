dfc408358fe28b995d88661935282ab8ab5978aa
Raphaelsants <raphaelbjj8@gmail.com>
2022-11-29 15:39:57 -0300

    att2

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,27 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'; 
 import Navbar from './componentes/estaticos/navbar/Navbar';
 import Footer from './componentes/estaticos/footer/Footer';
 import Home from './paginas/home/Home';
-
+import Login from './paginas/login/Login'
 
 
 function App() {
   return (
     <>
-    <Navbar />
-    <Home />
-    <Footer />
+    <Router>
+    <Navbar/>
+    <Routes> 
+      
+      <Route path='/home' element={<Home />}/>
+
+      <Route path='/login' element={<Login />}/>
+
+    </Routes>
+    <Footer/>
+    </Router>
     </>
   );
 }
